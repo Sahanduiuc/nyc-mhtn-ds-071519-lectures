3e9761f8aa25ce5f60c32b40a0755e8d012a09f5
Marco Santos <marcosan93@gmail.com>
2019-07-24 09:44:48 -0400

    asdf

---
+++ week-2/SQL/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 15:16:25 2019
+
+@author: flatironschool
+"""
+
+host = 'flatiron.cxfspvbqp2rq.us-east-1.rds.amazonaws.com'
+user = 'marcosan93'
+password = 'gam3fr3ak'
